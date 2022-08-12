383d4603c0ced2859ca1480b7e27097c1d7e4340
kook4 <talantbekov0404@gmail.com>
2022-08-13 01:00:02 +0600

    origin

--- greeting.js
+++ greeting.js
@@ -15,21 +15,64 @@
 
 
 
-let str = ''
-for (let i = 0; i <= 7; i++){
-    console.log(str)
-    str += '*'
+// let str = ''
+// for (let i = 0; i <= 7; i++){
+//     console.log(str)
+//     str += '*'
+// }
+//
+// let num = 1;
+// for (; num < 100; num++){
+//     if(num % 5 === 0 && num % 3 === 0) {
+//         console.log('Fizzbuzz' + num)
+//     }else if (num % 5 === 0) {
+//         console.log('Buzz' + num)
+//     }else if (num%3 === 0){
+//         console.log('Fizz'+num)
+//     }else{
+//         console.log(num)
+//     }
+// }
+
+
+let getReturn = function (age) {
+    return (age > 18) ? "true" : "Родители разрешили?"
+}
+console.log(getReturn(18))
+
+
+let result = function (a, b){
+    return (a + b < 4) ? "мало" : "много"
+}
+console.log(result(1,1))
+
+
+let messages = function (login) {
+    return(login === "сотрудник") ? "Привет" : (login === "директор") ? "Здравствуйте" : (login === "") ? "нет логина" : ""
+}
+console.log(messages("директор"))
+
+ 
+let a = [0]
+let ggg
+if (a > 0){
+    console.log("1")
+} else{
+    console.log("2")
 }
 
-let num = 1;
-for (; num < 100; num++){
-    if(num % 5 === 0 && num % 3 === 0) {
-        console.log('Fizzbuzz' + num)
-    }else if (num % 5 === 0) {
-        console.log('Buzz' + num)
-    }else if (num%3 === 0){
-        console.log('Fizz'+num)
-    }else{
-        console.log(num)
-    }
+const nick = ["nick",true,false,50]
+const kick = ["nick",true,false,45]
+console.log(nick.length)
+console.log(kick.length)
+
+nick.length =
+kick.length = 6
+function arr(height){
+    return(nick)
 }
+console.log(nick, kick)
+
+
+let element = (element) => console.log ('Hydrogen', 'Helium', 'Lithium', 'Beryllium')
+console.log(element)
